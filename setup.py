0509dc95988aa90a1ede5e63b0655fea8fb1f573
Harsh Desai <hrdesai007@gmail.com>
2019-09-03 13:07:59 -0500

    :heavy_plus_sign:

--- setup.py
+++ setup.py
@@ -15,12 +15,12 @@ with open(readme_path, encoding='utf-8') as f:
 
 setup(
     name='super_awesome_eda',
-    version = '0.0.1',
+    version='0.0.1',
     packages=['awesome_eda'],
     description='Awesome EDA API to make EDA <-> Modeling process easier',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    author = 'Harsh Desai',
-    author_email = 'hurshd0@gmail.com',
+    author='Harsh Desai',
+    author_email='hurshd0@gmail.com',
     license='MIT'
 )
